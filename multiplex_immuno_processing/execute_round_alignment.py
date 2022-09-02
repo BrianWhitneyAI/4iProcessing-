e34875d4b454dc32edaa66d672c60e27baeadf02
Goutham Nadarajan <gouthamr@buffalo.edu>
2022-09-02 10:20:51 -0700

    add missing file

--- multiplex_immuno_processing/execute_round_alignment.py
+++ multiplex_immuno_processing/execute_round_alignment.py
@@ -3,7 +3,7 @@ from glob import glob
 import os
 from pathlib import Path
 import re
-
+import registration_utils
 from aicsimageio import AICSImage
 import numpy as np
 import pandas as pd
@@ -11,49 +11,46 @@ from scipy.ndimage import affine_transform
 import skimage.io as skio
 import yaml
 from yaml.loader import SafeLoader
-
-overwrite = True
-
-
-import registration_utils
-
-def os_swap(x):
-    out = "/" + ("/".join(x.split("\\"))).replace("//", "/")
-    return out
-
-
-
+import tqdm
 
 parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--dfall_csv_dir",
+    type=str,
+    required=True,
+    help="csv file",
+)
+
+parser.add_argument(
+    "--barcode", type=str, required=True, help="specify barcode to analyze"
+)
+
 parser.add_argument(
     "--output_path",
     type=str,
     required=True,
     help="output dir of all processing steps. This specifies where to find the yml_configs too",
 )
-parser.add_argument(
-    "--barcode", type=str, required=True, help="specify barcode to analyze"
-)
 
 parser.add_argument(
-    "-p",
-    "--position_list",
-    nargs='*',
+    "--position",
     type=str,
-    required=False,
-    help="specify positions to process. E.g. -p P1 P2"
+    required=True,
+    help="position"
 )
 
+parser.add_argument("--method", choices=['cross_cor', 'ORB'])
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    dfall = pd.read_csv(args.dfall_csv_dir)
+    mag = "20x"
 
-    # Open the file and load the file
-    # Open the file and load the file
-    barcode = args.barcode
+    Position = args.position
 
     yaml_dir = os.path.join(args.output_path, "yml_configs")
+    barcode = args.barcode
 
     yaml_list = [x for x in os.listdir(yaml_dir) if "_confirmed" in x]
     yaml_list = [x for x in yaml_list if barcode in x]
@@ -70,196 +67,133 @@ if __name__ == "__main__":
                 dfsub["output_path"] = data["output_path"]
                 dflist.append(dfsub)
 
-    dfconfig = pd.concat(dflist)
-    dfconfig.set_index(["barcode", "round"], inplace=True)
-
-    mag = "20x"
-
-    output_dir = dfconfig["output_path"][0]
-    pickle_dir = output_dir + os.sep + "pickles"
-    pickle_name = barcode + "cleanedup_match_csv.csv"
-    pickle_path = pickle_dir + os.sep + pickle_name
-    print("\n\n" + pickle_path + "\n\n")
-    # dfall = pd.read_pickle(pickle_path)
-    dfall = pd.read_csv(pickle_path)
+        dfconfig = pd.concat(dflist)
+        dfconfig.set_index(["barcode", "round"], inplace=True)
 
 
-    output_dir = dfconfig["output_path"][0]
-    align_pickle_dir = output_dir + os.sep + "alignment_pickles_each"
-    align_pickle_name_glob = f"{barcode}*alignment_csv_each.csv"
-    print(align_pickle_name_glob)
-    globlist = glob(align_pickle_dir + os.sep + align_pickle_name_glob)
-    dfalign_list = []
-    for align_pickle_path in globlist:
-        # align_pickle_path = align_pickle_dir + os.sep + align_pickle_name
-        df = pd.read_csv(align_pickle_path)
-        dfalign_list.append(df)
-    dfalign = pd.concat(dfalign_list)
 
-    dfall["parent_file"] = dfall["parent_file"].apply(lambda x: os_swap(x))
 
-    # merge both dataframes so that you only try to align the positions that can be aligned.
-    dfall = pd.merge(
-        dfalign,
-        dfall,
-        on=["key", "template_position"],
-        suffixes=("_align", ""),
-        how="left",
-    )
 
-    keylist = dfall["key"].unique()
-    # for Position in ['P2']:
-
-    if args.position_list is not None:
-        template_position_list = [x for x in np.sort(dfall["template_position"].unique()) if x in args.position_list]
-        print('choosing subset of positions = ', args.position_list)
+    keylist = dfall.set_index("template_position").loc[args.position, "key"].unique()
+        # testing_keylist = [x for x in keylist if "Time" not in x]
+    # print(testing_keylist)
+    # for ki, key in enumerate(testing_keylist):
+    if args.method=='cross_cor':
+        alignment_method = "alignment_offsets_xyz_cross_corr"
     else:
-        template_position_list = np.sort(dfall["template_position"].unique())
+        alignment_method = "alignment_offsets_xyz_ORB"
+    
+    
+    count=0
+    for ki, key in enumerate(keylist):
 
-    print(template_position_list)
-    # go one position by position, since you need offsets per position
-    for Position in template_position_list:  
+        print(f"On Image {count} of {keylist}")
+        print(key)
+        count+=1
 
-        print("POSITION = ", Position)
+        dfr = dfall.set_index(["template_position", "key"])
+        dfsub = dfr.loc[pd.IndexSlice[[Position], [key]], :]
+        parent_file = dfsub["parent_file"][0]
 
-        # need to define the keylist for each position, since some positions may not be imaged every round
-        keylist = dfall.set_index("template_position").loc[Position, "key"].unique()
-        # keylist = [x for x in keylist if "Time" not in x]
-        # print(testing_keylist)
-        # for ki, key in enumerate(testing_keylist):
-        for ki, key in enumerate(keylist):
-            print(key)
 
-            dfr = dfall.set_index(["template_position", "key"])
-            dfsub = dfr.loc[pd.IndexSlice[[Position], [key]], :]
-            parent_file = dfsub["parent_file"][0]
-
-            alignment_offset = eval(
-                dfalign.set_index(["key", "template_position"]).loc[
-                    pd.IndexSlice[key, Position], "alignment_offsets_zyx"
+        alignment_offset = eval(
+            dfall.set_index(["key", "template_position"]).loc[
+                pd.IndexSlice[key, Position], alignment_method
+            ]
+        )
+        print(alignment_offset)
+        print(type(alignment_offset))
+        final_shape = np.uint16(
+            np.asarray(
+                [
+                    100,
+                    1248 + 1248 / 3,
+                    1848 + 1848 / 3,
                 ]
             )
-            print(alignment_offset)
-            print(type(alignment_offset))
+        )
 
-            
-            zpad = 20
-            xypad = 300
-            final_shape = np.uint16(
-                np.asarray(
-                    [
-                        45 + zpad*2,
-                        1248 + xypad*2,
-                        1848 + xypad*2,
-                    ]
-                )
-            )
+        reader = AICSImage(parent_file)
 
-            reader = AICSImage(parent_file)
+        scene = dfsub["Scene"][0]
 
-            scene = dfsub["Scene"][0]
+        # 3 get variables for file name
+        # round_num0 = re.search("time|Round [0-9]+", parent_file, re.IGNORECASE).group(0)
+        search_out = re.search("time|Round [0-9]+", parent_file, re.IGNORECASE)
+        assert search_out is not None  # necessary for passing mypy type errors
+        round_num0 = search_out.group(0)
 
-            # # 3 get variables for file name
-            # # round_num0 = re.search("time|Round [0-9]+", parent_file, re.IGNORECASE).group(0)
-            # search_out = re.search("time|Round [0-9]+", parent_file, re.IGNORECASE)
-            # assert search_out is not None  # necessary for passing mypy type errors
-            # round_num0 = search_out.group(0)
+        round_num = round_num0.replace("Time", "0").replace("Round ", "").zfill(2)
 
-            # round_num = round_num0.replace("Time", "0").replace("Round ", "").zfill(2)
-            round_num = str(dfr.loc[pd.IndexSlice[Position,key],'round_number']).zfill(2)
+        scene_num = str(scene).zfill(2)
+        position_num = Position[1::].zfill(2)
+        well = parent_file = dfsub["Well_id"][0]
+        channels = reader.channel_names
 
-            scene_num = str(scene).zfill(2)
-            position_num = Position[1::].zfill(2)
-            well = dfsub["Well_id"][0]
-            channels = reader.channel_names
+        position = Position
+        si = int(scene) - 1  # scene_index
 
-            position = Position
-            si = int(scene) - 1  # scene_index
+        reader.set_scene(si)
 
-            reader.set_scene(si)
+        try:
+            Tn = reader.dims["T"][0]  # choose last time point
+            notcorrupt = True
+        # except ValueError("This is a Corrupted Scene.") as e:
+        except Exception as e:
+            print(str(e), position)
+            notcorrupt = False
 
-            try:
-                Tn = reader.dims["T"][0]  # choose last time point
-                notcorrupt = True
-            # except ValueError("This is a Corrupted Scene.") as e:
-            except Exception as e:
-                print(str(e), position)
-                notcorrupt = False
+        if notcorrupt:
+            for T in range(Tn):
 
-            if notcorrupt:
-                for T in range(Tn):
+                for ci, c in enumerate(channels):
+                    delayed_chunk = reader.get_image_dask_data("ZYX", T=T, C=ci)
+                    imgstack = delayed_chunk.compute()
 
-                    for ci, c in enumerate(channels):
-                        sep = os.sep
-                        channel = c
-                        channel_num = str(ci + 1).zfill(2)
-                        tnum = str(T).zfill(3)
-                        savedir = (
-                            f"{output_dir}{sep}mip_exports{sep}{barcode}{sep}"
-                        )
+                    # this is where the alignment is performed
+                    print(alignment_offset)
+                    align_matrix = registration_utils.get_align_matrix(alignment_offset)
+                    shift_to_center_matrix = registration_utils.get_shift_to_center_matrix(
+                        imgstack.shape, final_shape
+                    )
+                    combo = shift_to_center_matrix @ align_matrix
+
+                    # aligned image
+                    processed_volume = affine_transform(
+                        imgstack,
+                        np.linalg.inv(combo),
+                        output_shape=final_shape,
+                        order=0,  # order = 0 means no interpolation...juust use nearest neighbor
+                    )
+
+                    cropped_maxz = np.max(
+                        processed_volume, axis=0
+                    )  # compute max z projection
+
+                    # now save this image
+                    output_dir = dfconfig["output_path"][0]
+
+                    sep = os.sep
+                    channel = c
+                    channel_num = str(ci + 1).zfill(2)
+                    tnum = str(T).zfill(3)
+                    savedir = os.path.join(args.output_path, 'mip_exports', f"{barcode}-export")
 
 
-                        fovid = f"{barcode}_R{round_num}_P{position_num}"
-                        savename = (
-                            f"{fovid}-mip-c{channel_num}_T{tnum}.tif"
-                        )
-                        savepath = os.path.abspath(f"{savedir}{sep}{savename}")
-                        
+                    if not os.path.exists(savedir):
+                        os.makedirs(savedir)
+                        print("making", os.path.abspath(savedir))
 
-                        if not os.path.exists(savepath):
+                    file_name_stem = Path(dfsub["parent_file"].iloc[0]).stem
 
-                            # print(savepath)
-                            # print(os.path.exists(savepath))
+                    fovid = f"{barcode}_R{round_num}_P{position_num}"
+                    savename = (
+                        f"{fovid}-mip-c{channel_num}_T{tnum}.tif"
+                    )
 
-                            
-                            # print(fovid)
-                            # print(scene,si,T,ci)
-                            # print(dfsub)
-                            # print(reader.dims)
-                            # print(reader.current_scene)
-                            # print(reader.current_scene_index)
-                            # print(parent_file)
-                            # print(dfsub["parent_file"][0])
-
-                            delayed_chunk = reader.get_image_dask_data("ZYX", T=T, C=ci)
-                            imgstack = delayed_chunk.compute()
-
-                            # this is where the alignment is performed
-                            print(alignment_offset)
-                            align_matrix = registration_utils.get_align_matrix(alignment_offset)
-                            shift_to_center_matrix = registration_utils.get_shift_to_center_matrix(
-                                imgstack.shape, final_shape
-                            )
-                            combo = shift_to_center_matrix @ align_matrix
-
-                            # aligned image
-                            processed_volume = affine_transform(
-                                imgstack,
-                                np.linalg.inv(combo),
-                                output_shape=final_shape,
-                                order=0,  # order = 0 means no interpolation...juust use nearest neighbor
-                            )
-
-                            cropped_maxz = np.max(
-                                processed_volume, axis=0
-                            )  # compute max z projection
-
-                            # now save this image
-                            output_dir = dfconfig["output_path"][0]
-
-                            
-
-                            if not os.path.exists(savedir):
-                                os.makedirs(savedir)
-                                print("making", os.path.abspath(savedir))
-
-                            file_name_stem = Path(dfsub["parent_file"].iloc[0]).stem
-
-                            
-                            skio.imsave(
-                                savepath, np.uint16(cropped_maxz), check_contrast=False
-                            )
-                            if (T == 0) & (ci == 0):
-                                print(os.path.abspath(savepath))
-                        else:
-                            print(f"already processed{savename}")
+                    savepath = f"{savedir}{sep}{savename}"
+                    skio.imsave(
+                        savepath, np.uint16(cropped_maxz), check_contrast=False
+                    )
+                    if (T == 0) & (ci == 0):
+                        print(os.path.abspath(savepath))
