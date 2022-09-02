b92e0c9488d855a866cd8e558a66474f78faab22
Chris Frick <chris.frick@alleninstitute.org>
2022-09-02 11:45:17 -0700

    refactors generate_alignment_parameters to save method, failed, and cross_corr_xyz

--- multiplex_immuno_processing/generate_alignment_paramters.py
+++ multiplex_immuno_processing/generate_alignment_paramters.py
@@ -1,28 +1,22 @@
 import argparse
 import os
-import skimage.exposure as skex
 from aicsimageio import AICSImage
-import matplotlib as plt
 import numpy as np
 import pandas as pd
 import yaml
 from yaml.loader import SafeLoader
 import registration_utils
-import tifffile
 
 overwrite = True
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "--dfall_csv_dir",
-    required=True,
-    type=str,
-    help="csv file",
-)
 
 parser.add_argument(
-    "--barcode", required=True, type=str, help="specify barcode to analyze"
+    "--barcode",
+    required=True,
+    type=str,
+    help="specify barcode to analyze"
 )
 
 parser.add_argument(
@@ -39,7 +33,17 @@ parser.add_argument(
     help="position"
 )
 
-parser.add_argument("--method", choices=['cross_cor', 'ORB', 'both'])
+
+parser.add_argument(
+    "--test_save",
+    type=str,
+    required=False,
+    help="modify output directory by appending 'test' (default='')",
+    default = '',
+)
+
+parser.add_argument("--method",
+            choices=['cross_cor', 'ORB', 'both'])
 
 
 if __name__ == "__main__":
@@ -95,12 +99,9 @@ if __name__ == "__main__":
 
         reader = AICSImage(parent_file)
 
-        # channel_dict = get_channels(czi)
         channels = reader.channel_names
-        # print('channels found = ', channels)
 
         align_channel = dfsub["align_channel"][0]
-        # print('align_channel found = ', align_channel)
 
         position = Position
         scene = dfsub["Scene"][0]
@@ -108,7 +109,7 @@ if __name__ == "__main__":
         align_channel_index = [
             xi for xi, x in enumerate(channels) if x == align_channel
         ][0]
-        # print(position,' - S' + str(scene).zfill(3) )
+
         si = int(scene) - 1  # scene_index
 
         reader.set_scene(si)
@@ -129,7 +130,6 @@ if __name__ == "__main__":
             imgstack = delayed_chunk.compute()
             #TODO: There is an issue where everything in z is not being loaded for the very first image...... is this just inconsistent imageing or is this an AICSIMAGEIO issue???
             print(f"imgstack is of shape {np.shape(imgstack)}")
-            # print(imgstack.shape)
             keeplist.append((align_channel, key, imgstack))
 
     # now align images
@@ -138,85 +138,127 @@ if __name__ == "__main__":
 
     dfimg = pd.DataFrame(keeplist, columns=["align_channel", "key", "img"])
     dfimg.set_index("key", inplace=True)
-    # dfimg.loc['Round 1'][['align_channel']]
 
     keylist = dfimg.index.values
 
-    img_list = []
+    
+    reference_round_key = "Round 1"
+    refimg = dfimg.loc[reference_round_key, "img"]
+    
+
+    alignment_offsets_xyz_list = []
+    failed_list = []
+    final_version_offsets_zyx_list=[]
+    cross_corr_offset_zyx_list =[]
+    method_list = []
+
+    # iterate through each round and append the outcomes to the lists above
     for key in keylist:
         align_channel = dfimg.loc[key, "align_channel"]
         imgstack = dfimg.loc[key, "img"]
-        img_list.append(imgstack.copy())
-    print(len(img_list))
-    print(f"img list is of type {type(img_list)}")
-    print(f"shape of img list is {np.shape(img_list)}")
-    print(f"first image in list is of shape {np.shape(img_list[1])}")
 
-    # tifffile.imwrite("img_list_0.tiff", img_list[1])
-    # tifffile.imwrite("img_list_1.tiff", img_list[2])
+        # try to align with current code
+        try:
+            (_, _, meanoffset, _,) = registration_utils.find_xyz_offset(
+                refimg.copy(), imgstack.copy(), ploton=False, verbose=False,
+            )
+            failed = False
+        except Exception as e:
+            print(str(e))
+            failed = True
+            print(f"{barcode}-{Position}-{key} has failed alignment")
+            meanoffset = [0,0,0]
+        
 
-
-
-    reference_round_key = "Round 1"
-    refimg = dfimg.loc[reference_round_key, "img"]
-    alignment_offsets_xyz_list = registration_utils.find_xyz_offset_relative_to_ref(
-        img_list, refimg=refimg, ploton=False, verbose=False
-    )
-    source_cropping=100
-    if args.method == "ORB" or args.method=="both":
-        print(f"alignment offset xyz is {alignment_offsets_xyz_list}")
-
-        #target_img_padded = np.pad(refimg, ((0, 0), (50, 50), (50, 50)), mode='constant')
-        # tifffile.imwrite("refimg.tiff", refimg)
-        alignment_offsets_xyz_list_ORB= []
-
-
-        for i in range(len(img_list)):
+        cross_corr_offset_zyx_list.append(meanoffset)
+        
+        
+        if args.method == "ORB" or args.method=="both":
+            source_cropping=100
+            
             try:
-                print(f"looking at {i}")
-                new_source_img = img_list[i]
-                source_img_FOV_cropped = new_source_img[:, source_cropping:np.shape(new_source_img)[1]-source_cropping, source_cropping:np.shape(new_source_img)[2]-source_cropping]
+                new_source_img = imgstack
+                source_img_FOV_cropped = new_source_img[:,
+                                                        source_cropping:np.shape(new_source_img)[1]-source_cropping,
+                                                        source_cropping:np.shape(new_source_img)[2]-source_cropping]
 
-                #source_img_padded = np.pad(source_img_FOV, ((0, 0), (50, 50), (50, 50)), mode='constant')
+                
                 print(f"shape of ref img is {np.shape(refimg)}")
                 print(f"shape of source image is {np.shape(source_img_FOV_cropped)}")
-                # tifffile.imwrite(f"source_img_FOV_{i}_padded.tiff", source_img_FOV_cropped)
-
+                
                 #ref_img_cropped = refimg[:,100:np.shape(refimg)[1]-100, 100:np.shape(refimg)[2]-100]
                 # the indexing here is z,y,x -- check this
-                final_y_offset, final_x_offset = registration_utils.perform_alignment(source_img_FOV_cropped, refimg, smaller_fov_modality="source", scale_factor_xy=1, scale_factor_z=1, source_alignment_channel=0, target_alignment_channel=0, source_output_channel=[0], target_output_channel=[0], prealign_z=True, denoise_z=True, use_refinement=False, save_composite=False)
-                print(final_y_offset, final_x_offset)
-                # match_list = align_helper.return_aligned_img_list_new(img_list,offset_list)
+                final_y_offset, final_x_offset = registration_utils.perform_alignment(source_img_FOV_cropped,
+                                                                                        refimg,
+                                                                                        smaller_fov_modality="source",
+                                                                                        scale_factor_xy=1,
+                                                                                        scale_factor_z=1,
+                                                                                        source_alignment_channel=0,
+                                                                                        target_alignment_channel=0,
+                                                                                        source_output_channel=[0],
+                                                                                        target_output_channel=[0],
+                                                                                        prealign_z=True,
+                                                                                        denoise_z=True,
+                                                                                        use_refinement=False,
+                                                                                        save_composite=False)
+
+                # need to adjust offset for the source_cropping performed above
+                final_y_offset = source_cropping - final_y_offset
+                final_x_offset = source_cropping - final_x_offset
+                
                 print("for first img in new_source_img, x_offset is", final_x_offset)
                 print("for first img in new_source_img, y_offset is", final_y_offset)
-                alignment_offsets_xyz_list_ORB.append((final_x_offset, final_y_offset))
+
             except:
-                alignment_offsets_xyz_list_ORB.append((0,0))
+                final_x_offset = 0
+                final_y_offset = 0
+                failed=True
+            
+            alignment_offsets_xyz_ORB = (final_x_offset, final_y_offset)
 
-        final_version_offsets_zyx=[]
-        # This just copies over the z offset from the cross correlation method
-        for k in range(len(alignment_offsets_xyz_list_ORB)):
-            dat=alignment_offsets_xyz_list_ORB[k]
-            final_version_offsets_zyx.append([alignment_offsets_xyz_list[k][0], source_cropping-dat[0], source_cropping-dat[1]])
-        
-        print(f"final version offset xyz is {final_version_offsets_zyx}")
-        print("outputing")
-        dfimg["alignment_offsets_xyz_ORB"] = final_version_offsets_zyx
-        dfimg["alignment_offsets_xyz_cross_corr"] = alignment_offsets_xyz_list
+            #now this method collects the alignment in Z from the cross correlation method
+            final_version_offsets_zyx = [meanoffset[0],
+                                        alignment_offsets_xyz_ORB[0],
+                                        alignment_offsets_xyz_ORB[1],
+                                        ]
 
-    elif args.method=="cross_cor":
-        dfimg["alignment_offsets_xyz_cross_corr"] = alignment_offsets_xyz_list
-        dfimg["alignment_offsets_xyz_ORB"] = [np.NaN for f in alignment_offsets_xyz_list]
-        
+            method = 'ORB'
+            
+            
+            
+            print(f"final version offset xyz is {final_version_offsets_zyx}")
+            print("outputing")
+
+
+        elif args.method=="cross_cor":
+            final_version_offsets_zyx = meanoffset
+            method = 'cross_cor'
+            # failed is determined above
+
+            
+
+        # record outcomes
+        final_version_offsets_zyx_list.append(final_version_offsets_zyx)
+        method_list.append(method)
+        failed_list.append(failed)
+
+
+
+    dfimg['alignment_offsets_xyz'] = final_version_offsets_zyx_list
+    dfimg['method'] = method_list
+    dfimg['failed'] = failed_list
+    dfimg['alignment_offsets_xyz_cross_cor'] = cross_corr_offset_zyx_list
+            
 
 
     print("dfimg.shape[0] is {}".format(dfimg.shape[0]))
     dfimg["template_position"] = [Position] * dfimg.shape[0] # This is what?
-    dfout_p = dfimg[["template_position", "align_channel", "alignment_offsets_xyz_ORB", "alignment_offsets_xyz_cross_corr"]]
+    dfout_p = dfimg[["template_position", "align_channel", "alignment_offsets_xyz", "alignment_offsets_xyz_cross_cor","method"]]
     #dfkeeplist.append(dfout_p)
 
+
     output_dir = dfconfig["output_path"][0]
-    pickle_dir = output_dir + os.sep + "alignment_pickles_each"
+    pickle_dir = output_dir + os.sep + "alignment_pickles_each" + args.test_save
     if not os.path.exists(pickle_dir):
         os.makedirs(pickle_dir)
     pickle_name = f"{barcode}-{Position}-alignment_pickle_each.pickle"
